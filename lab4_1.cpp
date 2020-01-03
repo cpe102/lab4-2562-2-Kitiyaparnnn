417d05c75071777ac7d181ccf222def8f4290486
lab314-14@labeng.local <lab314-14@labeng.local>
2020-01-03 14:51:16 +0700

    final

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+int main()
+{
+    double x,y,e=2.71828;
+    cout<<"Enter x =";
+    cin>>x;
+    y=(3*x*x*x)+(2*e)+pow(2,(2*x+1))+sqrt(x*x+1);
+    cout<<"Result y ="<<y;
+    return 0;
+}
