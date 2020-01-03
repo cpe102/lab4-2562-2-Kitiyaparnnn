ad2afa6848fe602216dc813fa6823a037efd9f28
lab314-14@labeng.local <lab314-14@labeng.local>
2020-01-03 15:05:16 +0700

    final

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -3,10 +3,17 @@
 using namespace std;
 
 //Write function findDistance() here
+double findDistance(double u,double a,double t)
+{
+  double s;
+    s=(u*t)+0.5*a*(t*t);
+    return s;
+}
 
 int main(){
 
   //Calling findDistance() using test cases
+  cout<<"Distance is "<<findDistance(5,4,3);
 
   return 0;
 }
